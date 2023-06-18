915e0e199b4be40578c5e18551464be08b5ab01e
AM <adim93d@gmail.com>
2023-06-19 01:57:42 +0300

    First commit, pytest TDD

---
+++ test_product.py
@@ -0,0 +1,30 @@
+import pytest
+import products
+
+"""
+Test that creating a normal product works.
+Test that creating a product with invalid details (empty name, negative price) invokes an exception.
+Test that when a product reaches 0 quantity, it becomes inactive.
+Test that product purchase modifies the quantity and returns the right output.
+Test that buying a larger quantity than exists invokes exception.
+"""
+
+
+def test_create_product():
+    pass
+
+
+def test_create_invalid_product():
+    pass
+
+
+def test_no_inventory():
+    pass
+
+
+def test_modified_quantity():
+    pass
+
+
+def test_larger_than_quantity_exception():
+    pass
